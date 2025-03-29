f09f34f31a65c39cafc598f8d3b7752e6f77bf62
jonascarvalh <olaffic@gmail.com>
2025-03-29 17:43:47 -0300

    feat: adicionando controle de tempo e sons

--- alura-include/semana-8/Forkus/script.js
+++ alura-include/semana-8/Forkus/script.js
@@ -5,10 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+
+const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('sons/luna-rise-part-one.mp3');
-musica.loop = true;
+const musicaPlay = new Audio('sons/play.wav');
+const musicaPause = new Audio('sons/pause.mp3');
+const musicaEnd = new Audio('sons/beep.mp3');
 
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
 musicaFocoInput.addEventListener('change', () => {
     if (musica.paused) {
         musica.play();
@@ -52,10 +60,31 @@ function alterarContexto(contexto) {
             break;
     }
 }
-const displayTempo = document.querySelector('#timer');
 
-const botaoIniciar = document.querySelector('.app__card-primary-button');
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        musicaEnd.play();
+        zerar();
+        alert('Tempo finalizado');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ', tempoDecorridoEmSegundos);
+}
 
-const duracaoFoco = 1500;
-const duracaoDescansoCurto = 300;
-const duracaoDesancsoLongo = 900;
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        musicaPause.play();
+        zerar();
+        return;
+    }
+    musicaPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
