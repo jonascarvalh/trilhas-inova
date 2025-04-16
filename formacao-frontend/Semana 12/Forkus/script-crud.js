c778d15469854e31f6bf2abc2578be41f63f37e8
Jonas <jonas.sousa@citiesoft.com.br>
2025-04-16 14:33:06 -0300

    feat: adicionando funcionalidade de remover itens completos e remover todos

--- formacao-frontend/Semana 12/Forkus/script-crud.js
+++ formacao-frontend/Semana 12/Forkus/script-crud.js
@@ -4,7 +4,12 @@ const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas');
+const btnRemoverTodos = document.querySelector('#btn-remover-todas');
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
 
@@ -112,3 +117,15 @@ document.addEventListener('FocoFinalizado', () => {
         atualizarTarefas();
     }
 });
+
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item";
+    document.querySelectorAll(seletor).forEach((elemento) => {
+        elemento.remove();
+        tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : [];
+        atualizarTarefas();
+    })
+}
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btnRemoverTodos.onclick = () => removerTarefas(false);
