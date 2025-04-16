5152f41a178c36368ce1c57de13315ad16f52da0
Jonas <jonas.sousa@citiesoft.com.br>
2025-04-16 08:47:12 -0300

    feat: criando projeto forkus e editando nome da pasta

--- formacao-frontend/Semana 1/alura_books-projeto_inicial/app/metodoSort.js
+++
@@ -1,9 +0,0 @@
-let btnOrdernarPorPreco = document.getElementById("btnOrdenarPorPreco");
-btnOrdernarPorPreco.addEventListener('click', ordernarLivrosPorPreco);
-
-function ordernarLivrosPorPreco() {
-    let livrosOrdenados = livros.sort((a, b) =>
-        a.preco - b.preco
-    );
-    exibirOsLivrosNaTela(livrosOrdenados);
-}
